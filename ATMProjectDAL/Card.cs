693eaa0aa7cf545c79ee14384192a631d56089bf
ryu609 <m.joynauth@gmail.com>
2017-12-25 09:26:54 +0400

    Solution SetUp

---
+++ ATMProjectDAL/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMProjectDAL
+{
+    public class Card
+    {
+        public string Pin { get; set; }
+        public bool IsReported { get; set; }
+        public string CardNumber { get; set; }
+    }
+}
