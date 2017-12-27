8d46ff252472ba5ada8e4376a379dc4c57af2cce
ryu609 <m.joynauth@gmail.com>
2017-12-27 16:57:43 +0400

    Clean project and bundled angular

--- ATMProject/Global.asax.cs
+++ ATMProject/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ATMProject
@@ -12,6 +13,7 @@ namespace ATMProject
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
