9e4cd7102b78a0969967eea3b06efab4dc213a95
ryu609 <m.joynauth@gmail.com>
2017-12-27 19:22:27 +0400

    Routing with authentication works

---
+++ ATMProject/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ATMProject
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Angular")
+                .Include("~/Scripts/angular.js")
+                .Include("~/Scripts/angular-route.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ATMProject")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .Include("~/Scripts/ATMProject.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ ATMProject/Scripts/ATMProject.js
@@ -0,0 +1,15 @@
+﻿var atmproject = angular.module('ATMProject', ['ngRoute']);
+atmproject.controller('LandingPageController', LandingPageController);
+
+var configFunction = function($routeProvider) {
+    $routeProvider
+        .when('/routeOne', { templateUrl: 'routesDemo/one' })
+        .when('/routeTwo/:donuts', {
+            templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }})
+        .when('/routeThree', { templateUrl: 'routeDemo/three' });
+        
+}
+
+configFunction.$inject = ['$routeProvider'];
+
+atmproject.config(configFunction)
