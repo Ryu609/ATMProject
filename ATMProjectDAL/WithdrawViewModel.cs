fba6cdc5014d66a64494e3cbb47bc96bbe7319ab
ryu609 <m.joynauth@gmail.com>
2017-12-30 09:08:06 +0400

    Withdraw ViewModel

---
+++ ATMProjectDAL/WithdrawViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMTestDAL
+{
+    public class WithdrawViewModel
+    {
+        public int Amount { get; set; }
+        public string CardNumber { get; set; }
+    }
+}
